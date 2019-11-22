74cece886a27e4c0f0caa80d894afa4e8ea634e6
Kirill <hotasice41@gmail.com>
2019-11-22 11:25:10 +0100

    правки по pylint & pep8

--- 1/main.py
+++ 1/main.py
@@ -13,8 +13,9 @@ def get_html(url):
 def get_data(html):
     """Преобразуем html код в дерево объектов python """
     soup = BeautifulSoup(html, 'lxml')
-    h1 = soup.find('div', id='home-welcome').find('header').find('h1').text
-    return h1
+    find_header = soup.find(
+        'div', id='home-welcome').find('header').find('h1').text
+    return find_header
 
 
 def main():
